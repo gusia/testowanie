cb6896f54a43265fe527aaa4191af1773aa3935c
Krzysztof Mataj <gumik0@gmail.com>
2011-05-29 01:37:05 +0200

    Nowy projekt z bibliotekami do grafów.

---
+++ Goofy/src/pl/edu/uj/ii/goofy/Main.java
@@ -0,0 +1,15 @@
+package pl.edu.uj.ii.goofy;
+
+import org.jgrapht.demo.JGraphAdapterDemo;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Goofy!");
+		JGraphAdapterDemo.main(args);
+	}
+
+}
