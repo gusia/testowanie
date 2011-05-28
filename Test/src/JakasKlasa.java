fbf96db27855922f46d4ea69c39b84a5845cd96c
Jadwiga Piotrowska <jadzia.piotrowska@gmail.com>
2011-05-28 13:14:29 +0200

    Przykładowy projekt Java w Eclipse. Plik .gitignore w zasadzie do użycia w głównym projekcie.

---
+++ Test/src/JakasKlasa.java
@@ -0,0 +1,12 @@
+
+public class JakasKlasa {
+	private int property;
+
+	public void setProperty(int property) {
+		this.property = property;
+	}
+
+	public int getProperty() {
+		return property;
+	}
+}
