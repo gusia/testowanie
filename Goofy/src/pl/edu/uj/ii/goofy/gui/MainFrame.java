f211b75e876a75702486ebc15233c15d53b2054e
Jadwiga Piotrowska <jadzia.piotrowska@gmail.com>
2011-06-17 00:15:44 +0200

    Zmienione okienko Main i zrobione początkowe
    :(

--- Goofy/src/pl/edu/uj/ii/goofy/gui/MainFrame.java
+++ Goofy/src/pl/edu/uj/ii/goofy/gui/MainFrame.java
@@ -1,43 +1,57 @@
 package pl.edu.uj.ii.goofy.gui;
 
 import java.awt.Component;
-import java.awt.Dialog.ModalExclusionType;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import net.miginfocom.swing.MigLayout;
 import edu.uci.ics.jung.algorithms.layout.CircleLayout;
 import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.graph.DirectedSparseGraph;
-import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.visualization.BasicVisualizationServer;
 import edu.uci.ics.jung.visualization.VisualizationServer;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
 
 public class MainFrame extends JFrame {
 
+	private static final long serialVersionUID = 740477122217644965L;
+	private List<String> wierzcholkiPoczatkowe = new LinkedList<String>();
+	private List<String> wierzcholkiKoncowe = new LinkedList<String>();
 	private JPanel contentPane;
 	private JPanel panel;
 	private DirectedSparseGraph<String, Integer> graf;
-	Layout<String, Integer> layout;
-	BasicVisualizationServer<String, Integer> vv;
+	private Layout<String, Integer> layout;
+	private VisualizationServer<String, Integer> vv;
+	
+	
+	public List<String> getWierzcholkiPoczatkowe() {
+		return wierzcholkiPoczatkowe;
+	}
+
+
+	public List<String> getWierzcholkiKoncowe() {
+		return wierzcholkiKoncowe;
+	}
+
 
 	public DirectedSparseGraph<String, Integer> getGraf() {
 		return graf;
 	}
 
-	public void setGraf(DirectedSparseGraph<String, Integer> graf) {
-		this.graf = graf;
-	}
-
+	
 	/**
 	 * Launch the application.
 	 */
@@ -62,14 +76,13 @@ public class MainFrame extends JFrame {
 		layout = new CircleLayout<String, Integer>(graf);
 		layout.setSize(new Dimension(300, 400));
 		vv = new BasicVisualizationServer<String, Integer>(layout);
-		vv.setPreferredSize(new Dimension(300, 400));
+		((JComponent) vv).setPreferredSize(new Dimension(300, 500));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 703, 518);
+		setBounds(100, 100, 881, 614);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[fill][grow][grow][grow]",
-				"[fill][][][][grow]"));
+		contentPane.setLayout(new MigLayout("", "[::150,fill][::150,fill][fill][::200,fill][::700,grow]", "[fill][][][][][grow][]"));
 
 		JButton btnDodajWierzchoki = new JButton("Dodaj wierzchołki");
 		btnDodajWierzchoki.addActionListener(new ActionListener() {
@@ -77,23 +90,20 @@ public class MainFrame extends JFrame {
 				dodajWierzcholki();
 			}
 		});
-		contentPane.add(btnDodajWierzchoki, "cell 0 0");
+		contentPane.add(btnDodajWierzchoki, "cell 0 0 4 1");
 
 		panel = new JPanel();
-		contentPane.add(panel, "cell 1 0 3 5,grow");
-		panel.add(vv);
+		contentPane.add(panel, "cell 4 0 1 7,grow");
+		panel.add((Component) vv);
 		
 		JButton btnWierzchokiPocztkowekocowe = new JButton(
-				"Wierzchołki początkowe/końcowe");
+				"Oznacz wierzchołki początkowe/końcowe");
 		btnWierzchokiPocztkowekocowe.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				PoczatkoweFrame pf = new PoczatkoweFrame();
-				System.out.println("asdf");
-				pf.setVisible(true);
-				System.out.println("asdfasdf");
+				oznaczPoczatkoweKoncoweWierzcholki();			
 			}
 		});
-		contentPane.add(btnWierzchokiPocztkowekocowe, "cell 0 1");
+		contentPane.add(btnWierzchokiPocztkowekocowe, "cell 0 1 4 1");
 
 		JButton btnDodajKrawdzie = new JButton("Dodaj krawędzie");
 		btnDodajKrawdzie.addActionListener(new ActionListener() {
@@ -101,7 +111,40 @@ public class MainFrame extends JFrame {
 				dodajKrawedzie();
 			}
 		});
-		contentPane.add(btnDodajKrawdzie, "cell 0 2");
+		contentPane.add(btnDodajKrawdzie, "cell 0 2 4 1");
+		
+		JLabel lblNewLabel = new JLabel("Kryterium:");
+		contentPane.add(lblNewLabel, "cell 0 3,alignx trailing");
+		
+		JComboBox comboBox = new JComboBox();
+		contentPane.add(comboBox, "cell 1 3 2 1,growx");
+		
+		JButton btnNewButton = new JButton("Pokaż");
+		contentPane.add(btnNewButton, "cell 3 3,growx");
+		
+		JLabel lblNewLabel_1 = new JLabel("Wymagania testowe:");
+		contentPane.add(lblNewLabel_1, "cell 0 4 2 1");
+		
+		JLabel lblNewLabel_2 = new JLabel("Ścieżki testowe:");
+		contentPane.add(lblNewLabel_2, "cell 2 4 2 1");
+		
+		JScrollPane scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, "cell 0 5 2 1,grow");
+		
+		JList list = new JList();
+		scrollPane.setViewportView(list);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		contentPane.add(scrollPane_1, "cell 2 5 2 1,grow");
+		
+		JList list_1 = new JList();
+		scrollPane_1.setViewportView(list_1);
+		
+		JButton btnNewButton_1 = new JButton("Pokaż wymagania na grafie");
+		contentPane.add(btnNewButton_1, "cell 0 6 2 1");
+		
+		JButton btnNewButton_2 = new JButton("Pokaż ścieżki na grafie");
+		contentPane.add(btnNewButton_2, "cell 2 6 2 1,growx");
 	}
 
 	void dodajWierzcholki() {
@@ -115,6 +158,12 @@ public class MainFrame extends JFrame {
 		KrawedzieFrame kf = new KrawedzieFrame(this);
 		kf.setVisible(true);
 		kf.setModal(true);
+	}
+	
+	void oznaczPoczatkoweKoncoweWierzcholki(){
+		PoczatkoweFrame pf = new PoczatkoweFrame(this);
+		pf.setVisible(true);
+		pf.setModal(true);				
 	}
 
 }
--- Goofy/src/pl/edu/uj/ii/goofy/gui/PoczatkoweFrame.java
+++ Goofy/src/pl/edu/uj/ii/goofy/gui/PoczatkoweFrame.java
@@ -1,82 +1,196 @@
 package pl.edu.uj.ii.goofy.gui;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Enumeration;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import net.miginfocom.swing.MigLayout;
-import javax.swing.JList;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
 
-public class PoczatkoweFrame extends JFrame {
+import net.miginfocom.swing.MigLayout;
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+
+public class PoczatkoweFrame extends JDialog {
 
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					PoczatkoweFrame frame = new PoczatkoweFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	MainFrame mFrame;
+	DirectedSparseGraph<String, Integer> graf;
+	private JList list_2;
+	private JList list_1;
+	private JList list;
 
 	/**
 	 * Create the frame.
 	 */
-	public PoczatkoweFrame() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public PoczatkoweFrame(MainFrame _mFrame) {
+		mFrame = _mFrame;
+		graf = mFrame.getGraf();
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[grow][][grow][][grow]", "[][grow][grow][]"));
-		
+		contentPane.setLayout(new MigLayout("", "[grow][][grow][][grow]",
+				"[][grow][grow][]"));
+
 		JLabel lblNewLabel = new JLabel("Startowe:");
 		contentPane.add(lblNewLabel, "cell 0 0");
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Wszystkie:");
 		contentPane.add(lblNewLabel_1, "cell 2 0");
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Końcowe:");
 		contentPane.add(lblNewLabel_2, "cell 4 0");
-		
-		JList list = new JList();
-		contentPane.add(list, "cell 0 1 1 2,grow");
-		
+
 		JButton btnNewButton = new JButton("<-");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dodajDoPocztakowych();
+			}
+		});
+
+		JScrollPane scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, "cell 0 1 1 2,grow");
+
+		list = new JList();
+		scrollPane.setViewportView(list);
 		contentPane.add(btnNewButton, "cell 1 1,growx,aligny bottom");
-		
-		JList list_1 = new JList();
-		contentPane.add(list_1, "cell 2 1 1 2,grow");
-		
+		list.setModel(new DefaultListModel());
+		for (String wierzcholek : mFrame.getWierzcholkiPoczatkowe()) {
+			((DefaultListModel) list.getModel()).addElement(wierzcholek);
+		}
+
 		JButton btnNewButton_2 = new JButton("->");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dodajDoKoncowych();
+			}
+		});
+
+		JScrollPane scrollPane_1 = new JScrollPane();
+		contentPane.add(scrollPane_1, "cell 2 1 1 2,grow");
+
+		list_1 = new JList();
+		scrollPane_1.setViewportView(list_1);
 		contentPane.add(btnNewButton_2, "cell 3 1,growx,aligny bottom");
-		
-		JList list_2 = new JList();
-		contentPane.add(list_2, "cell 4 1 1 2,grow");
-		
+		list_1.setModel(new DefaultListModel());
+		for (String wierzcholek : graf.getVertices()) {
+			((DefaultListModel) list_1.getModel()).addElement(wierzcholek);
+		}
+
 		JButton btnNewButton_1 = new JButton("->");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				usunZPoczatkowych();
+			}
+		});
+
+		JScrollPane scrollPane_2 = new JScrollPane();
+		contentPane.add(scrollPane_2, "cell 4 1 1 2,grow");
+
+		list_2 = new JList();
+		scrollPane_2.setViewportView(list_2);
 		contentPane.add(btnNewButton_1, "cell 1 2,growx,aligny top");
-		
+		list_2.setModel(new DefaultListModel());
+		for (String wierzcholek : mFrame.getWierzcholkiKoncowe()) {
+			((DefaultListModel) list_2.getModel()).addElement(wierzcholek);
+		}
+
 		JButton btnNewButton_3 = new JButton("<-");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				usunZKoncowych();
+			}
+		});
 		contentPane.add(btnNewButton_3, "cell 3 2,growx,aligny top");
-		
+
 		JButton btnZapisz = new JButton("Zapisz");
+		btnZapisz.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				zapiszZmiany();
+			}
+		});
 		contentPane.add(btnZapisz, "cell 0 3 2 1,growx");
-		
+
 		JButton btnAnuluj = new JButton("Anuluj");
+		btnAnuluj.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		});
 		contentPane.add(btnAnuluj, "cell 3 3 2 1,growx");
+	}
+
+	private void dodajDoKoncowych() {
+		try {
+			Integer index = list_1.getSelectedIndex();
+			String el = (String) ((DefaultListModel) list_1.getModel())
+					.getElementAt(index);
+			if (!((DefaultListModel) list_2.getModel()).contains(el)) {
+				((DefaultListModel) list_2.getModel()).addElement(el);
+			}
+		} catch (Exception e) {
+		}
+
+	}
+
+	private void usunZKoncowych() {
+		try {
+			Integer index = list_2.getSelectedIndex();
+			((DefaultListModel) list_2.getModel()).removeElementAt(index);
+		} catch (Exception e) {
+		}
+	}
+
+	private void dodajDoPocztakowych() {
+		try {
+			Integer index = list_1.getSelectedIndex();
+			String el = (String) ((DefaultListModel) list_1.getModel())
+					.getElementAt(index);
+			if (!((DefaultListModel) list.getModel()).contains(el)) {
+				((DefaultListModel) list.getModel()).addElement(el);
+			}
+		} catch (Exception e) {
+		}
+	}
+
+	private void usunZPoczatkowych() {
+		try {
+			Integer index = list.getSelectedIndex();
+			((DefaultListModel) list.getModel()).removeElementAt(index);
+		} catch (Exception e) {
+		}
+	}
+
+	private void zapiszZmiany() {
+		Enumeration <String> enumer = (Enumeration<String>) ((DefaultListModel) list.getModel()).elements();
+		mFrame.getWierzcholkiPoczatkowe().clear();
+		mFrame.getWierzcholkiKoncowe().clear();
+		while (enumer.hasMoreElements()){
+			mFrame.getWierzcholkiPoczatkowe().add(enumer.nextElement());
+		}
+		enumer = (Enumeration<String>) ((DefaultListModel) list_2.getModel()).elements();
+		while (enumer.hasMoreElements()){
+			mFrame.getWierzcholkiKoncowe().add(enumer.nextElement());
+		}
+		System.out.println("Początkowe: \n");
+		for (String s : mFrame.getWierzcholkiPoczatkowe()){
+			System.out.print(s +", ");
+		}
+		System.out.println("Końcowe: \n");
+		for (String s : mFrame.getWierzcholkiKoncowe()){
+			System.out.print(s +", ");
+		}
+		
+		dispose();
+
+
 	}
 
 }
