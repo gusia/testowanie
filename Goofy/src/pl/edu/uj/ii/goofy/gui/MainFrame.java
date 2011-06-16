5899aa8b7bce8cc313f98b991b0e447a87635ff2
Jadwiga Piotrowska <jadzia.piotrowska@gmail.com>
2011-06-16 20:30:36 +0200

    Okienko dod wierzcholkow i glowne
    Okienko dodawania wierz działa ok(chyba), Okienko głowne ma biedne problem z wyświetlaniem grafu, ale zajmiemy się tym.

--- Goofy/src/pl/edu/uj/ii/goofy/gui/MainFrame.java
+++ Goofy/src/pl/edu/uj/ii/goofy/gui/MainFrame.java
@@ -1,6 +1,8 @@
 package pl.edu.uj.ii.goofy.gui;
 
 import java.awt.Component;
+import java.awt.Dialog.ModalExclusionType;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JButton;
@@ -11,6 +13,11 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import net.miginfocom.swing.MigLayout;
+import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.algorithms.layout.Layout;
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.visualization.BasicVisualizationServer;
 import edu.uci.ics.jung.visualization.VisualizationServer;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -19,6 +26,17 @@ public class MainFrame extends JFrame {
 
 	private JPanel contentPane;
 	private JPanel panel;
+	private DirectedSparseGraph<String, Integer> graf;
+	Layout<String, Integer> layout;
+	BasicVisualizationServer<String,Integer> vv;
+
+	public DirectedSparseGraph<String, Integer> getGraf() {
+		return graf;
+	}
+
+	public void setGraf(DirectedSparseGraph<String, Integer> graf) {
+		this.graf = graf;
+	}
 
 	/**
 	 * Launch the application.
@@ -40,6 +58,11 @@ public class MainFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public MainFrame() {
+		graf = new DirectedSparseGraph<String, Integer>();
+		layout = new CircleLayout<String, Integer>(graf);
+		layout.setSize(new Dimension(300,400));
+		vv = new BasicVisualizationServer<String,Integer>(layout);
+		vv.setPreferredSize(new Dimension(300,400));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 703, 518);
 		contentPane = new JPanel();
@@ -50,22 +73,24 @@ public class MainFrame extends JFrame {
 		JButton btnDodajWierzchoki = new JButton("Dodaj wierzchołki");
 		btnDodajWierzchoki.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				WierzcholkiFrame wf = new WierzcholkiFrame();
-				wf.setVisible(true);
-				setEnabled(false);
+				dodajWierzcholki();
 			}
 		});
 		contentPane.add(btnDodajWierzchoki, "cell 0 0");
 		
 		panel = new JPanel();
 		contentPane.add(panel, "cell 1 0 3 5,grow");
+		panel.add(vv);
+		test();
+		
 		
 		JButton btnWierzchokiPocztkowekocowe = new JButton("Wierzchołki początkowe/końcowe");
 		btnWierzchokiPocztkowekocowe.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				PoczatkoweFrame pf = new PoczatkoweFrame();
+				System.out.println("asdf");
 				pf.setVisible(true);
-				setEnabled(false);
+				System.out.println("asdfasdf");
 			}
 		});
 		contentPane.add(btnWierzchokiPocztkowekocowe, "cell 0 1");
@@ -75,13 +100,22 @@ public class MainFrame extends JFrame {
 			public void actionPerformed(ActionEvent arg0) {
 				KrawedzieFrame kf = new KrawedzieFrame();
 				kf.setVisible(true);
-				setEnabled(false);
 			}
 		});
 		contentPane.add(btnDodajKrawdzie, "cell 0 2");
 	}
 
-	public void setGraph(VisualizationServer<Integer, String> vs) {
-		panel.add((Component) vs);
+	void dodajWierzcholki(){
+			WierzcholkiFrame wf = new WierzcholkiFrame(this);
+			wf.setModal(true);
+			wf.setVisible(true);
+			layout.reset();
+			
+	}
+	
+	void test (){
+		graf.addVertex("a");
+		graf.addVertex("b");
+		graf.addVertex("c");
 	}
 }
--- Goofy/src/pl/edu/uj/ii/goofy/gui/WierzcholkiFrame.java
+++ Goofy/src/pl/edu/uj/ii/goofy/gui/WierzcholkiFrame.java
@@ -1,62 +1,72 @@
 package pl.edu.uj.ii.goofy.gui;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import net.miginfocom.swing.MigLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.JList;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.AbstractListModel;
-import javax.swing.ScrollPaneConstants;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.border.EmptyBorder;
 
-public class WierzcholkiFrame extends JFrame {
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+import edu.uci.ics.jung.graph.Graph;
+
+import net.miginfocom.swing.MigLayout;
+
+public class WierzcholkiFrame extends JDialog {
 
 	private JPanel contentPane;
 	private JTextField textField;
 	private JList list;
+	MainFrame mFrame;
+	DirectedSparseGraph<String, Integer> graf;
+	HashSet<String> do_dodania;
+	HashSet<String> do_usuniecia;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					WierzcholkiFrame frame = new WierzcholkiFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	
 
 	/**
 	 * Create the frame.
 	 */
-	public WierzcholkiFrame() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public WierzcholkiFrame(MainFrame _mFrame) {
+		mFrame = _mFrame;
+		graf = mFrame.getGraf();
+		do_dodania = new HashSet<String>();
+		do_usuniecia = new HashSet<String>();
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[210px][6px][204px]", "[15px][19px][25px][15px][121px][25px]"));
+		contentPane.setLayout(new MigLayout("", "[210px][6px,grow]", "[15px][19px][][15px][121px,fill][][25px]"));
 		
-		JLabel lblWpiszDodawaneusuwaneWierzchoki = new JLabel("Wpisz nazwy dodawanych/usuwanych wierzchołków:");
-		contentPane.add(lblWpiszDodawaneusuwaneWierzchoki, "cell 0 0 3 1,growx,aligny top");
+		JLabel lblWpiszDodawaneusuwaneWierzchoki = new JLabel("Wpisz nazwy dodawanych wierzchołków:");
+		contentPane.add(lblWpiszDodawaneusuwaneWierzchoki, "cell 0 0 2 1,growx,aligny top");
 		
 		textField = new JTextField();
-		contentPane.add(textField, "cell 0 1 3 1,growx,aligny top");
+		textField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_ENTER){
+					dodajWierzcholek();
+				}
+			}
+		});
+		contentPane.add(textField, "cell 0 1 2 1,growx,aligny top");
 		textField.setColumns(10);
 		
 		JButton btnNewButton = new JButton("Dodaj");
@@ -65,33 +75,104 @@ public class WierzcholkiFrame extends JFrame {
 				dodajWierzcholek();
 			}
 		});
-		contentPane.add(btnNewButton, "cell 0 2,alignx right,aligny top");
-		
-		JButton btnNewButton_1 = new JButton("Usuń");
-		contentPane.add(btnNewButton_1, "cell 2 2,alignx left,aligny top");
+		contentPane.add(btnNewButton, "cell 0 2 2 1,growx,aligny top");
 		
 		JLabel lblWierzchokiTwojegoGrafu = new JLabel("Wierzchołki Twojego grafu:");
-		contentPane.add(lblWierzchokiTwojegoGrafu, "cell 0 3 3 1,growx,aligny top");
+		contentPane.add(lblWierzchokiTwojegoGrafu, "cell 0 3 2 1,growx,aligny top");
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		contentPane.add(scrollPane, "cell 0 4 3 1,grow");
+		contentPane.add(scrollPane, "cell 0 4 2 1,grow");
 		
 		list = new JList();
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_DELETE){
+					usunWierzcholek();
+				}
+			}
+		});
 		list.setValueIsAdjusting(true);
 		list.setModel(new DefaultListModel());
-		scrollPane.setColumnHeaderView(list);
+		System.out.println(graf.getVertexCount());
+		
+		
+		for (String wierzcholek : graf.getVertices()) {
+			((DefaultListModel)list.getModel()).addElement(wierzcholek);
+		}
+		
+		scrollPane.setViewportView(list);
+		
+		JButton btnNewButton_1 = new JButton("Usuń zaznaczony");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				usunWierzcholek();
+			}
+		});
+		contentPane.add(btnNewButton_1, "cell 0 5 2 1,growx,aligny top");
 		
 		JButton btnNewButton_2 = new JButton("Zapisz");
-		contentPane.add(btnNewButton_2, "cell 0 5,alignx right,aligny top");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				zapiszWierzcholkiDoGrafu();
+			}
+		});
+		contentPane.add(btnNewButton_2, "cell 0 6,growx,aligny top");
 		
 		JButton btnNewButton_3 = new JButton("Anuluj");
-		contentPane.add(btnNewButton_3, "cell 2 5,alignx left,aligny top");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		});
+		contentPane.add(btnNewButton_3, "cell 1 6,growx,aligny top");
 	}
 	void dodajWierzcholek(){
-		((DefaultListModel)list.getModel()).addElement(textField.getText());
+		String el = textField.getText();
+		if (!((DefaultListModel)list.getModel()).contains(el)){
+			((DefaultListModel)list.getModel()).addElement(el);
+			if (do_usuniecia.contains(el)){
+				do_usuniecia.remove(el);
+			}
+			else {
+				do_dodania.add(el);
+			}
+			
+			textField.setText("");
+		} else {
+			JOptionPane.showMessageDialog(this, "Wierzchołek już istnieje!");
+		}
 	}
-	public JList getList() {
-		return list;
+	void usunWierzcholek(){
+		try {
+			Integer index = list.getSelectedIndex();
+			String el = (String)((DefaultListModel)list.getModel()).getElementAt(index);
+			((DefaultListModel)list.getModel()).removeElementAt(index);
+			if (do_dodania.contains(el)){
+				do_dodania.remove(el);
+			}
+			else {
+				do_usuniecia.add(el);
+			}
+		} catch (Exception e){
+			
+		}
+	}
+	
+	void zapiszWierzcholkiDoGrafu (){
+		Iterator<String> iter = do_usuniecia.iterator();
+		while (iter.hasNext()){
+			String vertex = iter.next();
+			graf.removeVertex(vertex);
+			System.out.print("a ");
+		}
+		iter = do_dodania.iterator();
+		while (iter.hasNext()){
+			String vertex = iter.next();
+			graf.addVertex(vertex);
+			System.out.print("b ");
+		}
+		mFrame.setGraf(graf);
+		dispose();
 	}
 }
