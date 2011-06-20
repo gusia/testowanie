7c4776cb313cce7421937fdf9f9f21e5b5f0097e
Krzysztof Mataj <gumik0@gmail.com>
2011-06-21 00:39:04 +0200

    DuPathsCoverage. Small refactor in PrimePathsCoverage.

--- Goofy/src/pl/edu/uj/ii/goofy/algorithm/coverage/DuPathsCoverage.java
+++ Goofy/src/pl/edu/uj/ii/goofy/algorithm/coverage/DuPathsCoverage.java
@@ -1,41 +1,51 @@
 package pl.edu.uj.ii.goofy.algorithm.coverage;
 
+import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.LinkedList;
 
 import pl.edu.uj.ii.goofy.algorithm.DuDescription;
 import pl.edu.uj.ii.goofy.algorithm.DuDescriptionType;
+import pl.edu.uj.ii.goofy.algorithm.Edge;
+import pl.edu.uj.ii.goofy.algorithm.Node;
 import edu.uci.ics.jung.graph.Graph;
 
-public class DuPathsCoverage<N, E> implements TestRequirementInt<N, E> {
-	
-	private Hashtable<N, LinkedList<DuDescription>> duNode;
-	private Hashtable<E, LinkedList<DuDescription>> duEdge;
-	private Graph<N, E> graph;
-	private LinkedList<LinkedList<N>> paths;
+public class DuPathsCoverage implements TestRequirementInt<Node, Edge> {
 
-	public DuPathsCoverage(Graph<N, E> graph, Hashtable<N, LinkedList<DuDescription>> duNode, Hashtable<E, LinkedList<DuDescription>> duEdge) {
+	private Graph<Node, Edge> graph;
+	private HashSet<LinkedList<Node>> paths;
+	private Hashtable<Short, LinkedList<DuDescription>> nodeUsages;
+	
+	public DuPathsCoverage(Graph<Node, Edge> graph) {
 		this.graph = graph;
-		this.duNode = duNode;
-		this.duEdge = duEdge;
-		this.paths = new LinkedList<LinkedList<N>>();
+		this.paths = new HashSet<LinkedList<Node>>();
+		
+		nodeUsages = new Hashtable<Short, LinkedList<DuDescription>>();
+		for (Node node : graph.getVertices()) {
+			nodeUsages.put(node.getId(), node.getUsages());
+		}
 	}
 	
 	@Override
-	public LinkedList<LinkedList<N>> getRequirement() {
-		for (N begin : duNode.keySet()) {
+	public LinkedList<LinkedList<Node>> getRequirement() {
+		for (Node begin : graph.getVertices()) {
 			int descriptionIndex = 0;
-			for (DuDescription duDescription : duNode.get(begin)) {
+			for (DuDescription duDescription : begin.getUsages()) {
 				if (duDescription.getType() == DuDescriptionType.Definition) {
 					
 					// nodes
-					for (N end : duNode.keySet()) {
+					for (Node end : graph.getVertices()) {
 						int duDescriptionEndIndex = 0;
-						for (DuDescription duDescriptionEnd : duNode.get(begin)) {
-							if (duDescriptionEnd.getName() == duDescription.getName()
-								&& (begin != end || duDescriptionEndIndex > descriptionIndex)) {
+						for (DuDescription duDescriptionEnd : end.getUsages()) {
+							if (duDescriptionEnd.getName() == duDescription.getName()) {
+								if (begin != end && duDescriptionEnd.getType() == DuDescriptionType.Definition) {
+									break;
+								}
 								
-								findDuPath(duDescription.getName(), begin, end);
+								if (duDescriptionEnd.getType() == DuDescriptionType.Usage
+									&& ((begin != end) || (begin == end && duDescriptionEndIndex < descriptionIndex))) {
+									findDuPath(duDescription.getName(), begin, end, false);
+								}
 							}
 							
 							++duDescriptionEndIndex;
@@ -43,10 +53,10 @@ public class DuPathsCoverage<N, E> implements TestRequirementInt<N, E> {
 					}
 					
 					// edges
-					for (E end : duEdge.keySet()) {
-						for (DuDescription duDescriptionEnd : duNode.get(begin)) {
+					for (Edge end : graph.getEdges()) {
+						for (DuDescription duDescriptionEnd : end.getUsages()) {
 							if (duDescriptionEnd.getName() == duDescription.getName()) {
-								findDuPath(duDescription.getName(), begin, graph.getEndpoints(end).getSecond());
+								findDuPath(duDescription.getName(), begin, graph.getEndpoints(end).getSecond(), true);
 							}
 						}
 					}
@@ -56,27 +66,45 @@ public class DuPathsCoverage<N, E> implements TestRequirementInt<N, E> {
 			}
 		}
 		
-		return paths;
+		return new LinkedList<LinkedList<Node>>(paths);
 	}
 	
-	private void findDuPath(String name, N begin, N end) {
-		LinkedList<N> beginPath = new LinkedList<N>();
+	private void findDuPath(String name, Node begin, Node end, boolean edge) {
+		LinkedList<Node> beginPath = new LinkedList<Node>();
 		beginPath.add(begin);
-		findSimplePaths(beginPath, end);
+		findSimplePaths(name, beginPath, end, edge);
 	}
 
-	private void findSimplePaths(LinkedList<N> begin, N end) {
-
-		for (N succ : graph.getSuccessors(begin.getLast())) {
-			if (!begin.contains(succ) || succ == end) {
-				LinkedList<N> newPath = new LinkedList<N>(begin);
+	private void findSimplePaths(String name, LinkedList<Node> begin, Node end, boolean edge) {
+		for (Node succ : graph.getSuccessors(begin.getLast())) {
+			if ((!begin.contains(succ) || end.equals(succ))
+					&& 
+					(end.equals(begin.getFirst()) || !containsDefinitionBeforeUse(nodeUsages.get(succ.getId()), name)
+							|| (edge && succ.equals(end))
+					)
+				) {
+				LinkedList<Node> newPath = new LinkedList<Node>(begin);
 				newPath.add(succ);
-				findSimplePaths(newPath, end);
+				findSimplePaths(name, newPath, end, edge);
 			}
 		}
 		
-		if (end == begin.getLast()) {
+		if (end.equals(begin.getLast()) && begin.size() > 1) {
 			paths.add(begin);
 		}
+	}
+	
+	private boolean containsDefinitionBeforeUse(LinkedList<DuDescription> list, String name) { 
+		for (DuDescription desc : list) {
+			if (desc.getName() == name) {
+				if (desc.getType() == DuDescriptionType.Definition) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
+		
+		return false;
 	}
 }
